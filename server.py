72b02e9f7f956d6611c50b7866821f628f5e26d0
Aliasz1 <mzakwanas@protonmail.com>
2024-02-05 23:01:14 +0800

    Added route to get business documents

--- server.py
+++ server.py
@@ -8,19 +8,33 @@ app = Flask(__name__)
 mongo_uri = "mongodb+srv://atlarusent:p2EQnKPeoyN4Xaet@main.zwuqy9k.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(mongo_uri)
 
+database = client.get_database("DB")
+collection = database.get_collection("Businesses")
+
 # Route to get all collections in the connected database
 @app.route('/')
 def get_all_collections():
     try:
-        database = client.get_database("DB")
-        collection = database.get_collection("Businesses")
-        
         # Retrieve all documents from the collection
         documents = list(collection.find())
         
         return jsonify({"data": documents})
     except Exception as e:
         return jsonify({"error": str(e)})
+    
+@app.route('/<business_id>', methods=['GET'])
+def get_company(business_id):
+    try:
+        document = collection.find_one({"businessID": business_id})
+        
+        if document:
+            # Convert ObjectId to string for JSON serialization
+            document['_id'] = str(document['_id'])
+            return jsonify({"status": "success", "document": document})
+        else:
+            return jsonify({"status": "error", "message": f"Document not found for businessID: {business_id}"}), 404
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
     # Use environment variables for host and port
